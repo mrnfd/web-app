6c9ecd7e973c9c9d4c51aa2e590f17449870d8d9
marianafyu <mariana24@ru.is>
2025-05-06 19:39:53 +0000

    homepage, login user type, catalogue

---
+++ castle_apartments/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'base.html')
+
+def catalogue(request):
+    return render(request, 'catalogue.html')
+
+def sellers(request):
+    return HttpResponse("sellers page")
+
+def my_offers(request):
+    return HttpResponse("log in to see your offers")
+
+def log_in(request):
+    return render(request, 'role_selection.html')
+
+def login_as_buyer(request):
+    return HttpResponse("log in as buyer page")
+
+def login_as_seller(request):
+    return HttpResponse("log in as seller page")
