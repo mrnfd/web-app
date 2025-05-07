c9fecdb7ba8bbcccb4bc3ada8f30a2e7d8dfe7d7
marianafyu <mariana24@ru.is>
2025-05-07 10:13:34 +0000

    lalalala

--- castle_apartments/settings.py
+++ castle_apartments/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-na3j42ovq8l!fn8^)+5$vjdf1*e#%2gh9md5hit9ix6)(^l#yc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
