2e0c145fe5e1aa26eff4cfa09d1d3fab1ebe73bb
Rami-AJ <thorbjorn21@ru.is>
2025-05-07 13:06:50 +0000

    added changes to catalouge views to use fake test data

--- Listings/views.py
+++ Listings/views.py
@@ -1,3 +1,43 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+properties = [
+    {
+        'id': 1,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 2,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 3,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 4,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+]
+# Create your views here.
+
+def index(request):
+    return render(request,"listing/listings.html",{
+        "properties":properties
+    })
+
+def get_listing_by_id(request):
+    listing = [x for x in properties if x.id == id][0]
+    return render(request,"/listings/listing_detail.html.",{
+        "listing":listing
+    })
--- castle_apartments/views.py
+++ castle_apartments/views.py
@@ -8,6 +8,12 @@ def index(request):
 def catalogue(request):
     return render(request, 'catalogue.html')
 
+def catalogue(request):
+    return render(request,"catalogue.html",{
+        "properties":properties
+    })
+
+
 def sellers(request):
     return HttpResponse("sellers page")
 
@@ -22,3 +28,33 @@ def login_as_buyer(request):
 
 def login_as_seller(request):
     return HttpResponse("log in as seller page")
+
+
+# FYRIR TEST 
+
+properties = [
+    {
+        'id': 1,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 2,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 3,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+    {
+        'id': 4,
+        'street': '123 Main St',
+        'number': '123',
+        'zip': '10001',
+    },
+]
