3c565ca0d7b013ef6cc24442848682ab231b035b
marianafyu <mariana24@ru.is>
2025-05-07 10:15:37 +0000

    installed apps

--- castle_apartments/settings.py
+++ castle_apartments/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Buyers',
+    "Buyers.apps.BuyersConfig",
+    "Listings.apps.ListingsConfig",
+    "Offers.apps.OffersConfig",
+    "Sellers.apps.SellersConfig",
+    "Transactions.apps.TransactionsConfig",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
