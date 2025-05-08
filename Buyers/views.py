7c1645eb529c39d4d1e2772b0c5b6adb30285c45
Rami-AJ <thorbjorn21@ru.is>
2025-05-08 09:31:32 +0000

    byrjaði á register og öðru login tengdu

--- Buyers/views.py
+++ Buyers/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def register(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        return render(request,'buyer/register.html', {
+            'form': UserCreationForm()
+        })
