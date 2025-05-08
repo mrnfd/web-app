3d25e5cc850406f7ffe543a06a3dfe81515f3690
IcelandicBoy <olafurv10@gmail.com>
2025-05-08 14:12:41 +0000

    Tvíkaði aðeins með profile images í seller/buyers models.py

--- Buyers/models.py
+++ Buyers/models.py
@@ -4,7 +4,7 @@ class Buyer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     contact_number = models.CharField(max_length=20)
-    profile_image = models.ImageField(upload_to="buyer_profiles/", null=True, blank=True)
+    profile_image_url = models.CharField(max_length=4096, blank = True)
 
     def __str__(self):
         return self.name
--- Buyers/views.py
+++ Buyers/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import  Buyer
 from .forms import BuyerProfileForm
 
 # Create your views here.
-
-def edit_profile(request, buyer_id):
+def edit_buyer_profile(request, buyer_id):
     buyer = get_object_or_404(Buyer, id=buyer_id)
 
     if request.method == "POST":
         form = BuyerProfileForm(request.POST, request.FILES, instance=buyer)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your profile was successfully updated!')
             return redirect('profile', buyer_id=buyer_id)
+        else:
+            messages.error(request, 'Please correct the error below.')
     else:
         form = BuyerProfileForm(instance=buyer)
-
-    return render(request, 'edit_profile.html', {'form': form})
+    return render(request, 'edit_buyer_profile.html', {'form': form})
