20544c5c5da856466bb9832565482fcfc6380d46
Rami-AJ <thorbjorn21@ru.is>
2025-05-12 13:14:27 +0000

    my offers functionality er komið an þess að gera það mjög ljótt

--- Buyers/views.py
+++ Buyers/views.py
@@ -7,6 +7,10 @@ from Offers.models import Offer
 from django.db.models import Q
 from django.http import HttpResponse, JsonResponse
 
+from Listings.models import Listing, ListingImage
+from Sellers.models import Seller
+
+
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
@@ -47,51 +51,79 @@ def my_offers(request):
     #all_offers = Offer.objects.get(buyer_id=request.user.id)
     #all_offers = Offer.objects.filter(buyer_id = id)
     all_offers = Offer.objects.all()
+    offer_array = []
 
     query_params = {
         'search_filter': request.GET.get('search_filter'),
         'sort': request.GET.get('sort'),
     }
-
-    if query_params['search_filter']:
-        search_filter = request.GET.get('search_filter', '')
-        if search_filter and search_filter != '':
-            applied_filter = True
-            all_offers = all_offers.filter(
-                Q(city__icontains=search_filter) | 
-                Q(house_numb__icontains=search_filter) | 
-                Q(zip_code__icontains=search_filter) |
-                Q(name__icontains=search_filter) |
-                Q(street__icontains=search_filter) 
-            )
-            # Return JSON response
-            print("Filters applied, returning JSON response")
+            
     # sort by
     sort_options = {
-        'price_asc': 'price',
-        'price_desc': '-price',
-        'street': 'street',
+        'ANY': 'ANY',
+        'ACCEPTED': 'ACCEPTED',
+        'PENDING': 'PENDING',
+        'CONTINGENT': 'CONTINGENT',
+        'REJECTED': 'REJECTED'
     }
-    if query_params['sort'] in sort_options:
+    if query_params['sort'] and query_params['sort'] in sort_options:
         applied_filter = True
-        all_offers = all_offers.filter(status= sort_options[query_params['sort']])    
+        if query_params['sort'] != 'ANY':
+            
+            all_offers = all_offers.filter(status=query_params['sort'])   
 
-    if applied_filter:        
-            return JsonResponse({
-                'offers': [{
-                    'id': offer.id,
-                    'name': offer.name,
-                    'seller_type': offer.seller_type,
-                    'street': offer.street,
-                    'house_numb': offer.house_numb,
-                    'city': offer.city,
-                    'profile_image_url': offer.profile_image_url
-                } for offer in all_offers]
-            })
+    
+    search_filter = request.GET.get('search_filter', '')
+    for offer in all_offers:
+        offer_listing = Listing.objects.get(id=offer.property_listing_id)
+        listing_seller = Seller.objects.get(id=offer_listing.seller_id_id)
+        
+        offer_info = {
+            'id': offer.id,
+            'price': str(offer.price),
+            'status': offer.status,
+            'submission_date':offer.submission_date,
+            'expiration_date':offer.expiration_date,
+
+            'listing_id': offer_listing.id,
+            'type': offer_listing.type,
+            'thumbnail': offer_listing.thumbnail,
+            'street': offer_listing.street,
+            'number': offer_listing.number,
+            'zip': offer_listing.zip,
+            'city': offer_listing.city,
+            
+            'seller': listing_seller.name,
+            'seller_id': listing_seller.id,
+        
+        }
+        
+        if search_filter and search_filter != '':
+            
+            applied_filter = True
+                       
+            #if search_filter in offer_info['street'] or search_filter in offer_info['number'] or search_filter in offer_info['zip'] or search_filter in offer_info['city']:
+            #    offer_array.append(offer_info) 
+            search_match = (
+                search_filter.lower() in offer_listing.street.lower() or
+                search_filter.lower() in str(offer_listing.number).lower() or
+                search_filter.lower() in str(offer_listing.zip).lower() or
+                search_filter.lower() in offer_listing.city.lower() or
+                search_filter.lower() in listing_seller.name.lower()
+            )
+            
+            if search_match:
+                offer_array.append(offer_info)
+        else:
+            offer_array.append(offer_info)
+
+    print(applied_filter)
+    if applied_filter:
+        return JsonResponse({'offers':offer_array})        
             
     # If no filter return normal
     return render(request, 'buyers/my_offers.html', {
-        "offers": all_offers
+        "offers": offer_array
     })
     
 
--- static/js/offer_filter.js
+++ static/js/offer_filter.js
@@ -2,65 +2,83 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     function registerSearchButtonHandler(){
-        // Register event handler for search button
+        
         const searchButton = document.getElementById('apply-filters')
-        // when clicked retrive filtered content
+        
         searchButton.addEventListener('click', async function(){
             const searchFilter = document.getElementById('search-value').value;
             const sortBy = document.getElementById('status-filter').value;
 
-            const propertyPlaceholder = document.getElementById('offer-listed')
+            const offerPlaceholder = document.getElementById('offer-listed')
             const url = new URL(window.location.href);
             url.search = ''; 
             
-            // Smiða url bara þeir filterar sem voru include-aðir
-            
+            // Construct URL only with included filters
             url.searchParams.append('search_filter', searchFilter || '');
             url.searchParams.append('sort', sortBy || '');
 
             const response = await fetch(url);
 
             if (response.ok){
-
-                // display filtered content
+                
                 const json = await response.json();
                 if (json.offers && Array.isArray(json.offers)) {
-                    const offerers = json.offers.map(offer => `
+                    const offerers = json.offers.map(offer => {
+                        
+                        let statusClass = 'pending';
+                        let statusText = offer.status;
+
+                        switch(offer.status) {
+                            case 'REJECTED':
+                                statusClass = 'rejected';
+                                break;
+                            case 'ACCEPTED':
+                                statusClass = 'accepted';
+                                break;
+                            case 'CONTINGENT':
+                                statusClass = 'contingent';
+                                break;
+                            default:
+                                statusClass = 'pending';
+                        }
+
+                        return `
                         <div class="property-card">
                             <div class="property-content">
-                                 <img src="https://i.pinimg.com/736x/d6/83/a0/d683a0dc882304dd58fa5d7c3038e096.jpg" alt="Property image">
+                                <img src="/static/${offer.thumbnail}" alt="Property image">
                                 <div class="property-details">
-                                    <p><strong>Fjallastræti 2, 110 Reykjavík, Single family home</strong></p>
-                                    <p><a href="#">Property details</a> </p>
+                                    <a href="/catalogue/${offer.listing_id}"> 
+                                        <strong>${offer.street} ${offer.house_numb}, ${offer.zip} ${offer.city}, ${offer.type}</strong>
+                                    </a>
+
                                     <p>Date of submission: ${offer.submission_date}</p>
                                     <p>Expiration date: ${offer.expiration_date}</p>
-                                    <p>Seller: Mia Wong</p>
-                                    <p>Purchase offer price: 200,000$</p>
-                                    <p>Status: <span class="status pending">Pending</span></p>
+                                    <p>Seller: ${offer.seller}</p>
+                                    <p>Purchase offer price: ${offer.price}$</p>
+
+                                    <p>Status: <span class="status ${statusClass}">${statusText}</span></p>
                                 </div>
                             </div>
 
                             <div class="buttons">
-                                <button type="button" id="cancel-btn">Cancel offer</button>
+                                <button type="button" id="cancel-btn">Delete offer</button>
                                 <button type="button">Resubmit offer</button>
                                 <button type="button" onclick="redirectToFinalize()">Finalize offer</button>
                             </div>
                         </div>
-                    `);
+                    `});
                     
                     // Update the content with the filtered properties
-                    propertyPlaceholder.innerHTML = offerers.join('');
+                    offerPlaceholder.innerHTML = offerers.join('');
                     
                 } else {
                     console.error('No offers match filter:', json);
-                    propertyPlaceholder.innerHTML = '<p> No offers found matching your filters.</p>';
+                    offerPlaceholder.innerHTML = '<p>No offers found matching your filters.</p>';
                 } 
             }
         });
-
     }
     registerSearchButtonHandler();
-
 
     const sortSelect = document.getElementById('status-filter');
     if (sortSelect) {
@@ -68,6 +86,4 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('apply-filters').click();
         });
     }
-    
 });
-
