2fae26e98d18937bf587708b933b95987dd5dc4c
marianafyu <mariana24@ru.is>
2025-05-16 10:43:30 +0000

    summary

--- Sellers/views.py
+++ Sellers/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Seller
-from .forms import SellerProfileForm
+#from .forms import SellerProfileForm, CreateSellerForm
+
 """
 # Create your views here.
 @login_required
@@ -21,20 +22,45 @@ def edit_seller_profile(request, seller_id):
         form = SellerProfileForm(instance=seller)
     return render(request, 'edit_seller_profile.html', {'form': form})
 """
+
+@login_required
 def seller_home(request):
     return render(request, 'seller/base_seller.html')
 
+@login_required
 def seller_offers(request):
     return render(request, 'seller/seller_offers.html')
 
+@login_required
 def seller_listings(request):
     return render(request, 'seller/manage_listings.html')
 
+@login_required
 def seller_add_listing(request):
     return render(request, 'seller/create_listing.html')
 
+@login_required
 def seller_edit_listing(request):
     return render(request, 'seller/seller_offers.html')
 
+@login_required
 def seller_profile(request):
-    return render(request, 'seller/seller_offers.html')
+    """
+    user_profile = Seller.objects.filter(user=request.user).first()
+
+    if request.method == "POST":
+        form = CreateSellerForm(request.POST, request.FILES, instance=user_profile)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            user_profile.save()
+            messages.success(request, 'Your profile was updated successfully!')
+            return redirect('seller_profile')
+        messages.error(request, 'Please correct the error below.')
+        return redirect('seller_profile')
+
+    return render(request, 'seller/seller_profile.html', {
+        'form': CreateSellerForm(instance=user_profile)})"""
+    return render(request, 'seller/seller_profile.html', {})
